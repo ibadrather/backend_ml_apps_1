fb7ad0552b7069ddb0926ed9a52b710971bad555
Ibad Rather <ibad.rather.ir@gmail.com>
2022-10-30 00:06:10 +0200

    xx

--- backend.py
+++ backend.py
@@ -1,9 +1,6 @@
 import numpy as np
 from fastapi import FastAPI, File, UploadFile
 import cv2
-import random
-import time
-import logging
 from starlette.responses import Response
 from backend_dependencies.bank_note_classification.bank_note_api_utils import (
     get_prediction,
