264256126cf006d6c97987625bc0eb0e94ba505d
Ibad Rather <ibad.rather.ir@gmail.com>
2022-10-29 22:57:46 +0200

    made backend simple and all apps working

--- backend_dependencies/translator_en_nl/translator_en_nl.py
+++ backend_dependencies/translator_en_nl/translator_en_nl.py
@@ -6,9 +6,11 @@ API_TOKEN = "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"
 API_URL = "https://api-inference.huggingface.co/models/Helsinki-NLP/opus-mt-en-nl"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
+
 def en_nl_query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
 class Payload(BaseModel):
     text: str
